ab3bdad03356dc0c37b65ec06163244e8730c5d7
Cory Bradbury <coradabra@gmail.com>
2023-11-01 15:03:55 +1100

    Add the majority of calculator functionality
    Add btnConfig array and helper functions to setup all buttons with less repetition
    
    Remove place holder text in index.html
    
    Add reducer for inputs, operator, clear, and evaluatue buttons

--- app.js
+++ app.js
@@ -1,52 +1,128 @@
-// const useState = require("./utils/state.js");
+const btnConfig = [
+  { selector: "#zero", action: "input", payload: "0" },
+  { selector: "#one", action: "input", payload: "1" },
+  { selector: "#two", action: "input", payload: "2" },
+  { selector: "#three", action: "input", payload: "3" },
+  { selector: "#four", action: "input", payload: "4" },
+  { selector: "#five", action: "input", payload: "5" },
+  { selector: "#six", action: "input", payload: "6" },
+  { selector: "#seven", action: "input", payload: "7" },
+  { selector: "#eight", action: "input", payload: "8" },
+  { selector: "#nine", action: "input", payload: "9" },
+  { selector: "#divide", action: "operator", payload: "divide" },
+  { selector: "#multiply", action: "operator", payload: "multiply" },
+  { selector: "#plus", action: "operator", payload: "plus" },
+  { selector: "#minus", action: "operator", payload: "minus" },
+  { selector: "#evaluate", action: "evaluate" },
+  { selector: "#clear", action: "clear", payload: "clear" },
+  { selector: "#negative-toggle", action: "toggle" },
+  { selector: "#percentage", action: "percentage" },
+];
 
-// import { useState } from "./utils/state";
+const operatorSymbols = {
+  plus: "+",
+  minus: "−",
+  multiply: "×",
+  divide: "÷",
+};
 
 const calculationDisplay = document.querySelector("#calculation");
 const inputDisplay = document.querySelector("#input");
 
-const clearBtn = document.querySelector("#clear");
-const toggleBtn = document.querySelector("#negative-toggle");
-const percentageBtn = document.querySelector("#percentage");
+let firstValue = "";
+let secondValue = "";
+let operator = "";
+let inputValue = inputDisplay.textContent;
 
-const divideBtn = document.querySelector("#divide");
-const multiplyBtn = document.querySelector("#multiply");
-const minusBtn = document.querySelector("#minus");
-const plusBtn = document.querySelector("#plus");
-const decimalBtn = document.querySelector("#decimal");
-const evaluateBtn = document.querySelector("#evaluate");
+const evaluate = () => {
+  const a = Number(firstValue);
+  const b = Number(secondValue);
 
-const nineBtn = document.querySelector("#nine");
-const eightBtn = document.querySelector("#eight");
-const sevenBtn = document.querySelector("#seven");
-const sixBtn = document.querySelector("#six");
-const fiveBtn = document.querySelector("#five");
-const fourBtn = document.querySelector("#four");
-const threeBtn = document.querySelector("#three");
-const twoBtn = document.querySelector("#two");
-const oneBtn = document.querySelector("#one");
-const zeroBtn = document.querySelector("#zero");
-
-// const [firstValue, setFirstValue] = state.useState("");
-// const [secondValue, setSecondValue] = useState("");
-// const [operator, setOperator] = useState("");
-
-const buttonReducer = (action, payload) => {
-  switch (action) {
-    case "input":
-      inputDisplay.textContent = inputDisplay.textContent + payload;
-    case "operator":
-    //do something
-    default:
+  switch (operator) {
+    case "plus":
+      inputValue = `${a + b}`;
+      inputDisplay.textContent = inputValue;
+      return;
+    case "minus":
+      inputValue = (a - b).toString();
+      inputDisplay.textContent = inputValue;
+      return;
+    case "multiply":
+      inputValue = (a * b).toString();
+      inputDisplay.textContent = inputValue;
+      return;
+    case "divide":
+      inputValue = (a / b).toString();
+      inputDisplay.textContent = inputValue;
       return;
   }
 };
 
-const addListeners = (button, action, payload) => {
+const setInputValue = (newValue, clear = false) => {
+  if (clear) {
+    inputValue = "0";
+    inputDisplay.textContent = inputValue;
+  } else if (inputValue === "0") {
+    inputValue = newValue;
+    inputDisplay.textContent = inputValue;
+  } else {
+    inputValue = inputValue + newValue;
+    inputDisplay.textContent = inputValue;
+  }
+};
+
+const setFirstValue = (newValue) => {
+  firstValue = newValue;
+  calculationDisplay.textContent = firstValue;
+};
+
+const setSecondValue = (newValue) => {
+  secondValue = newValue;
+  calculationDisplay.textContent = `${firstValue} ${operatorSymbols[operator]} ${secondValue} =`;
+};
+
+const setOperator = (newValue) => {
+  operator = newValue;
+  calculationDisplay.textContent = `${firstValue} ${operatorSymbols[operator]}`;
+};
+
+const buttonReducer = (action, payload) => {
+  switch (action) {
+    case "input":
+      setInputValue(payload);
+      return;
+    case "operator":
+      if (firstValue === "") {
+        setFirstValue(inputValue);
+        setInputValue("", true);
+        setOperator(payload);
+      } else {
+        setOperator(payload);
+        const answer = evaluate();
+        inputValue = answer;
+        inputDisplay.textContent = inputValue;
+      }
+      return;
+    case "evaluate":
+      setSecondValue(inputValue);
+      evaluate();
+      return;
+    case "clear":
+      setFirstValue("");
+      setSecondValue("");
+      setOperator("");
+      setInputValue("0", true);
+      return;
+  }
+};
+
+const configureButtons = (selector, action, payload = "") => {
+  const button = document.querySelector(selector);
   button.addEventListener("click", () => {
     buttonReducer(action, payload);
   });
 };
 
-addListeners(zeroBtn, "input", "0");
-addListeners(oneBtn, "input", "1");
+for (let btn of btnConfig) {
+  configureButtons(btn.selector, btn.action, btn.payload);
+}
